642dc2669ba56c2d15f4484fb0ec53d876cfb6a0
Don Naro <dnaro@redhat.com>
2023-09-18 17:46:34 +0100

    clean up

--- generate-preview.sh
+++ generate-preview.sh
@@ -16,5 +16,5 @@ sed -i -e 's|.adoc|.adoc\[leveloffset=+1\]|g' index.asciidoc
 sed -i '1iinclude::downstream/attributes/attributes.adoc\[\]' index.asciidoc
 sed -i '2i\\' index.asciidoc
 sed -i '3i= Content preview' index.asciidoc
-sed -i '4iIMPORTANT: This is a preview site of content that is in development and not supported.' index.asciidoc
+sed -i '4iIMPORTANT: This is a preview site of content that is in development and not supported. Visit link:https://www.redhat.com/en/technologies/management/ansible\[Red Hat Ansible Automation Platform\] for more information.' index.asciidoc
 sed -i '5i\\' index.asciidoc
